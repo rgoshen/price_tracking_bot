7fcccffef1ac64be40873b64b2d190ba047d806e
rgoshen <rick.goshen@gmail.com>
2020-12-20 10:13:11 -0700

    scraped price from amazon

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import requests
+import lxml
+from bs4 import BeautifulSoup
 
 product_url = "https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
+
+amazon_headers = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+}
+
+response = requests.get(url=product_url, headers=amazon_headers)
+
+soup = BeautifulSoup(response.content, "lxml")
+
+price = soup.find(id="priceblock_ourprice").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
